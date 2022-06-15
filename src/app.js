878a435ec248352fbbd100204d9e5a15cfe0faa3
tmayolgomila <tmayolgomila@gmail.com>
2022-06-15 19:55:51 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var who = ["the dog", "my granma", "his turtle", "my bird"];
+  var what = ["eat", "pissed", "crushed", "broked"];
+  var when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var quien = who[Math.floor(Math.random() * who.length - 1)];
+
+  var que = what[Math.floor(Math.random() * what.length - 1)];
+
+  var donde = when[Math.floor(Math.random() * when.length - 1)];
+
+  const element = document.getElementById("boton");
+  element.addEventListener("click", myFunction);
+  function myFunction() {
+    document.getElementById("excuse").innerHTML =
+      quien + " " + que + " " + donde;
+  }
 };
